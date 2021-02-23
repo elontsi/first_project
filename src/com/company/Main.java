cf2fb3692c86e5928ca7bee8d531de9e032ca03d
elontsi <44556964+elontsi@users.noreply.github.com>
2021-02-23 16:52:41 -0500

    working with maven and integrating nexus with jenkins

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String name = new String("Amigo");
+        System.out.println("My test is good");
+        System.out.println(name.toUpperCase());
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
